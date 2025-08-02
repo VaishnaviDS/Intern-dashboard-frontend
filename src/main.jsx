7ef7071772eecef429e9754194136ab0b2764c69
Vaishnavi Sakpal <142401260+VaishnaviDS@users.noreply.github.com>
2025-08-03 00:53:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-export const server='http://localhost:5000'
+export const server='https://intern-dashboard-server.onrender.com'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
